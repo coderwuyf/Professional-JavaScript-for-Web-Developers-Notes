c7b1b229cbcb7ec19b0ae6edf641d5677b8dcf0a
伍裕丰 <1651263422@qq.com>
2022-07-04 13:54:05 +0800

    提交

---
+++ Chapter_3/3_2关键字与保留字.js
@@ -0,0 +1,26 @@
+// 关键字与保留字
+
+// 关键字
+// js引擎代码进行词法分析、语法分析时会特殊处理的一些字符，表示控制语句的开始与结束或执行特定操作
+// ECMA-262(第六版)规定的关键字有
+// 用于控制语句中的(分支、循环)
+// if, else, switch, case, break, continue, for, while, do
+// 错误捕获处理
+// try, catch, finally, throw
+// 声明
+// 变量声明 var, const(这里没有let是因为在第六版中let作为严格模式下的保留字)
+// 函数声明 function
+// 类相关 new, class, extends, super
+// 操作
+// 对象 delete, in, with, this
+// 类型检测 instanceof, typeof
+// 函数 return, yield
+// 文件模块 import, export
+// 其他
+// default 可用于switch case中的默认分支，也可作为模块导出的默认内容export default
+// void
+
+// 保留字
+// 始终保留 enum
+// 严格模式下保留 implements, package, public, interface, protected, static, let, private
+// 模块中保留 await
